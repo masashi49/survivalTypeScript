5f7dc1ae8c0999c1d8a294fb8e3420fd9c9109fe
yamadamasashi <shisama94@gmail.com>
2021-12-02 09:33:12 +0900

    typeの勉強

---
+++ increment.js
@@ -0,0 +1,17 @@
+function increment(num) {
+    return num + 1;
+}
+console.log(increment(99));
+var namesss = "pikachu";
+var no = 25;
+var genre = "mouse pokémon";
+var height = 0.4;
+var weight = 6.0;
+var pikachu = {
+    namesss: namesss,
+    no: no,
+    genre: genre,
+    height: height,
+    weight: weight
+};
+var fururu = false;
---
+++ increment.ts
@@ -0,0 +1,39 @@
+function increment(num: number): number {
+    return num + 1;
+}
+console.log(increment(99));
+
+
+
+
+
+// typeは型エイリアス
+// stringやnumber、objectなどの型に、名前をつけることができる
+type hoge = string | number
+const fururu: hoge = "あいういえ"
+
+type yama = string | boolean | number
+const hujisan: yama = "富士山"
+
+// オブジェクトを作るときにtypeを宣言すると、その通りにしないとだめ
+type Wild = {
+    namesss: string;
+    no: number;
+    genre: string;
+    height: number;
+    weight: number;
+}
+
+const namesss: string = "pikachu";
+const no: number = 25;
+const genre: string = "mouse pokémon";
+const height: number = 0.4;
+const weight: number = 6.0;
+
+const pikachu: Wild = {
+    namesss,
+    no,
+    genre,
+    height,
+    weight
+}
