2d518f60899ee788d9d8e015e888d90139d67db6
yamadamasashi <shisama94@gmail.com>
2021-12-07 09:36:51 +0900

    プロミスおさらいい

---
+++ promise.js
@@ -0,0 +1,28 @@
+// 指定した`timeout`ミリ秒経過するまで同期的にブロックする関数
+function blockTime(timeout) {
+    var startTime = Date.now();
+    // `timeout`ミリ秒経過するまで無限ループをする
+    while (true) {
+        var diffTime = Date.now() - startTime;
+        if (diffTime >= timeout) {
+            return; // 指定時間経過したら関数の実行を終了
+        }
+    }
+}
+// console.log("1,処理を開始");
+// // blockTime(1000); // 他の処理を1000ミリ秒（1秒間）ブロックする
+// // console.log("この行が呼ばれるまで処理が1秒間ブロックされる");
+// setTimeout(() => {
+//     console.log("3.ブロック開始")
+//     blockTime(1000)// 1秒
+//     console.log("4.ブロックする処理完了")
+// },1000)
+// console.log("2.同期処理します")
+function errorPromise(message) {
+    return new Promise(function (resolve, reject) {
+        reject(new Error(message));
+    });
+}
+errorPromise("エラーハンドリング").then(undefined, function (error) {
+    console.log(error);
+});
