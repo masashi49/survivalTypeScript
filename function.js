186a66d8785931c0b17f5340e86212d4d335671e
yamadamasashi <shisama94@gmail.com>
2021-12-10 00:21:15 +0900

    callとapplyを理解した

--- function.js
+++ function.js
@@ -9,4 +9,15 @@ console.log(10 > 0 && 20);
 // 両方trueならtrue　どっちかいっこでもfalseならfalseだよ
 // 左側がtureなら、右側を評価しにいくよ。
 // 文字列はtrue扱いだよつまり
-console.log("cat" && "dog"); //の場合はcatがtrue扱いで、dogがかえるよ
+console.log("cat" && "dog"); //の場合はcatがtrue扱いで、右を評価しにいき、dogもtrueだから、最後に評価したdogがかえるよ
+var hoge = {
+    name: "ほげです",
+    say: function (word) {
+        return this.name + word;
+    }
+};
+console.log(hoge.say("うんこ"));
+var huga = {
+    name: "ふが出る"
+};
+console.log(hoge.say.apply(huga, ["っっっs"]));
--- function.ts
+++ function.ts
@@ -15,3 +15,17 @@ console.log(10 > 0 && 20)
 // 左側がtureなら、右側を評価しにいくよ。
 // 文字列はtrue扱いだよつまり
 console.log("cat" && "dog") //の場合はcatがtrue扱いで、右を評価しにいき、dogもtrueだから、最後に評価したdogがかえるよ
+
+var hoge = {
+    name: "ほげです",
+    say: function (word:string) {
+        return this.name + word;
+    }
+}
+
+console.log(hoge.say("うんこ"))
+var huga = {
+    name : "ふが出る"
+}
+console.log(hoge.say.call(huga,"callだうよ"))
+console.log(hoge.say.apply(huga,["applyだよ"]))
