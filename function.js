b684de440ef9aaeab27e9703629b87194553821f
yamadamasashi <shisama94@gmail.com>
2021-12-10 00:04:22 +0900

    論理積を理解した

--- function.js
+++ function.js
@@ -2,5 +2,11 @@ var numA = function (x) {
     return x * x;
 };
 console.log(numA(100));
-var numB = function (x) { return x * x; };
+var numB = function (x) { return 40 === x; };
 console.log(numB(40));
+// 論理積を理解した
+console.log(10 > 0 && 20);
+// 両方trueならtrue　どっちかいっこでもfalseならfalseだよ
+// 左側がtureなら、右側を評価しにいくよ。
+// 文字列はtrue扱いだよつまり
+console.log("cat" && "dog"); //の場合はcatがtrue扱いで、dogがかえるよ
--- function.ts
+++ function.ts
@@ -4,6 +4,14 @@ const numA = (x: number): number => {
 
 console.log(numA(100))
 
-const numB =(x:number):boolean => 40 === x
+const numB = (x: number): boolean => 40 === x
 
 console.log(numB(40))
+
+// 論理積を理解した
+console.log(10 > 0 && 20)
+
+// 両方trueならtrue　どっちかいっこでもfalseならfalseだよ
+// 左側がtureなら、右側を評価しにいくよ。
+// 文字列はtrue扱いだよつまり
+console.log("cat" && "dog") //の場合はcatがtrue扱いで、右を評価しにいき、dogもtrueだから、最後に評価したdogがかえるよ
