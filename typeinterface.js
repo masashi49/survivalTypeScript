f9a1ecf0b0d66ecd52ebfa022651cb613d244308
yamadamasashi <shisama94@gmail.com>
2021-12-10 21:31:53 +0900

    interfaceとtype

---
+++ typeinterface.js
@@ -0,0 +1,10 @@
+// 追加した宣言は全て取り込まないとエラーになる。
+var fransPan = {
+    calories: 10,
+    type: "2200"
+};
+var manoDon = {
+    calories: 200,
+    type: "やわらかい",
+    gram: "すごい"
+};
--- typeinterface.ts
+++ typeinterface.ts
@@ -1,3 +1,4 @@
+// interfaceは宣言を後からaddできる。
 interface Bread {
     calories: number
 }
@@ -5,6 +6,30 @@ interface Bread {
     type: string
 }
 
+// 追加した宣言は全て取り込まないとエラーになる。
 const fransPan: Bread = {
     calories: 10,
+    type: "2200"
+}
+
+// typeで↑と同じことをしようとすると,typeの重複となりエラーが出る。
+type MaboDofu = {
+    calories: number
+    type: string
+}
+// type MaboDofu = { //重複できない
+//     gram: string
+// }
+
+type Rice = {
+    calories: number
+    gram: string
+}
+
+type Mabodon = MaboDofu & Rice // 2つのタイプを持つ新たなtypeを作るには、&で繋げる必要がある(交差型(union)という)
+
+const manoDon: Mabodon = {
+    calories: 200,
+    type: "やわらかい",
+    gram:"すごい"
 }
