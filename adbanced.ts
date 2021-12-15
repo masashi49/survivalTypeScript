91e095bcf7682b0f5ec153f891fc8fb84f2604dc
yamadamasashi <shisama94@gmail.com>
2021-12-15 09:37:08 +0900

    説明をしてみる

--- adbanced.ts
+++ adbanced.ts
@@ -1,12 +1,24 @@
 export default function genericsAdvancedSample() {
-    type Map<T, U> = (array: T[], fn: (item: T) => U) => U[]
 
-    function map(array:string[], fn:number):number { // 宣言が長すぎる。
+    // 型宣言の形を流用したいなぁ。何度も同じようなtypeを宣言したくないなぁ。
+    function mapNunber(array: string[], fn: number): number {
         return Number()
     }
+    function mapString(array: number[], fn: string): string {
+        return String()
+    }
+    function mapArray(array: number[], fn: string): string[] {
+        return [String()]
+    }
+    function mapBool(array: number[], fn: string): boolean {
+        const result = !array
+        return result
+    }
 
+    type Map<T, U> = (array: T[], fn: (item: T) => U) => U[] // ジェネリクスを使って、型指定を受け取るようにしよ！
 
-    const mapStringsToNumbers: Map<string, number> = (array, fn) => {
+    
+    const mapStringsToNumbers: Map<string, number> = (array, fn) => { // さっきのジェネリクス type Map 使ってnumberを返そう！
         const reuslt = []
         for (let i = 0; i < array.length; i++) {
             const item = array[i]
@@ -18,7 +30,7 @@ export default function genericsAdvancedSample() {
     const numgers = mapStringsToNumbers(["23", "3324", "23"], (item) => Number(item))
     console.log("ジェネリクス文字から数字", numgers)
 
-    const mapNumberToStrings: Map<number, string> = (array, fn) => {
+    const mapNumberToStrings: Map<number, string> = (array, fn) => { // さっきのジェネリクス type Map 使ってstringを返そう！
         const reuslt = []
         for (let i = 0; i < array.length; i++) {
             const item = array[i]
